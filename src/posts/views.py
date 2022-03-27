3f0a5126cf0f8badcbb2d10b539184d8411a4c21
Morgan <morgan.nott@hotmail.com>
2022-03-27 14:27:17 +0200

    - feat/CRUD - Ajout de la fonctionnalité CREATE posts - Ajout de la fonctionnalité UPDATE posts - Ajout de la vue de la page d'accueil

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
-# Create your views here.
+from posts.models import BlogPost
+
+
+class BlogHome(ListView):
+    model = BlogPost
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        """
+        overload the get_queryset method to determine if user is logged in
+        :return: queryset of posts or queryset of posts with published status
+        """
+        queryset = super().get_queryset()
+        # Verify if the admin user is logged in, if so, show published and non-published posts
+        if self.request.user.is_authenticated:
+            return queryset
+        # filter only published posts for non-authenticated users
+        return queryset.filter(published=True)
+
+
+class BlogPostCreate(CreateView):
+    model = BlogPost
+    template_name = 'posts/blogpost_create.html'
+    fields = ['title', 'content', ]
+
+
+class BlogPostUpdate(UpdateView):
+    model = BlogPost
+    template_name = 'posts/blogpost_edit.html'
+    fields = ['title', 'content', 'published', ]

b9a5418e27a9116d3eed318ffc51b66a34066f0b
Morgan <morgan.nott@hotmail.com>
2022-03-29 18:31:07 +0200

    - Modifications de .gitignore WIP

--- src/configuration/db_configuration.py
+++
@@ -1,8 +0,0 @@
-
-class DbConfiguration:
-
-    ENGINE:str = 'django.db.backends.postgresql',
-    NAME: str = 'blog',
-    USER: str = 'blogadmin',
-    PASSWORD: str = '1234',
-    PORT: str = '5432'
